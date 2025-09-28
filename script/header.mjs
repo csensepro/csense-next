8112fce20210e7b92db9e4e9db8dccf15be7e3e6
csensepro <csensepro@wr.do>
2025-09-28 22:36:29 +0800

    fix:header更新

--- script/header.mjs
+++ script/header.mjs
@@ -1,14 +1,16 @@
 import PackageJSON from '../package.json' with { type: 'json' }
 
 export default `// ==UserScript==
-// @name         CSense
+// @name         CSense Next
 // @namespace    CSense
 // @version      ${PackageJSON.version}
 // @license      AGPL-3.0
 // @description  一个 CCW 安全审计工具
-// @author       FurryR
+// @author       FurryR & Csense Next
 // @match        https://www.ccw.site/*
 // @icon         https://m.ccw.site/community/images/logo-ccw.png
 // @grant        none
 // @run-at       document-start
+// @downloadURL https://update.greasyfork.org/scripts/550966/CSense.user.js
+// @updateURL https://update.greasyfork.org/scripts/550966/CSense.meta.js
 // ==/UserScript==`
